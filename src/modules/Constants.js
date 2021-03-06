0d69f3a9ec4ec23693cc38b6bd07e24df093a1d8
Jonas <51110394+JonasPonas@users.noreply.github.com>
2021-03-06 17:11:56 +0200

    Update Constants.js

--- src/modules/Constants.js
+++ src/modules/Constants.js
@@ -1,3 +1,3 @@
-export const ipAddress = "http://localhost:5004";
+export const ipAddress = "http://bad-day.today:5004";
 
 export const defaultImg = "https://www.literarytraveler.com/wp-content/uploads/2013/05/Vincent_van_Gogh_Self_Portrait_1887_ChicagoArtInstitute.jpg";
