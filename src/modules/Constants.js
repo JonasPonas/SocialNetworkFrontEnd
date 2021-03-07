3d6851b386d30c638c4e90a00c8a52c9d107af4b
Jonas <51110394+JonasPonas@users.noreply.github.com>
2021-03-07 22:39:37 +0200

    use secure api url

--- src/modules/Constants.js
+++ src/modules/Constants.js
@@ -1,3 +1,3 @@
-export const ipAddress = "https://bad-day.today:2053";
+export const ipAddress = "https://s.bad-day.today:2053";
 
 export const defaultImg = "https://www.literarytraveler.com/wp-content/uploads/2013/05/Vincent_van_Gogh_Self_Portrait_1887_ChicagoArtInstitute.jpg";
