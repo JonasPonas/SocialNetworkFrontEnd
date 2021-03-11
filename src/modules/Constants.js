74967dbaa342133310a17b16261d3beeb3181330
JonasPonas <zemaitis.jonas0@gmail.com>
2021-03-11 12:14:17 +0200

    added production env

--- src/modules/Constants.js
+++ src/modules/Constants.js
@@ -1,3 +1,9 @@
-export const ipAddress = "https://s.bad-day.today:2053";
+var ipAddress = "http://localhost:2053";
 
-export const defaultImg = "https://www.literarytraveler.com/wp-content/uploads/2013/05/Vincent_van_Gogh_Self_Portrait_1887_ChicagoArtInstitute.jpg";
+if (process.env.NODE_ENV == "production")
+  ipAddress = "https://s.bad-day.today:2053";
+
+const defaultImg =
+  "https://www.literarytraveler.com/wp-content/uploads/2013/05/Vincent_van_Gogh_Self_Portrait_1887_ChicagoArtInstitute.jpg";
+
+export { ipAddress, defaultImg };
